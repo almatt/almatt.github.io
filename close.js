4eb2872732f5632b48b6ac19b2fc6271d64514be
Almat Karibay <karibayevalmat@gmail.com>
2025-11-27 18:31:08 +0500

    Create close.js

---
+++ close.js
@@ -0,0 +1,17 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    window.addEventListener('beforeunload', (event) => {
+    const message = "Вы уверены, что хотите покинуть страницу? Несохраненные данные будут потеряны.";
+
+    event.preventDefault();
+    event.returnValue = message; // Для Chrome
+
+    return message;
+});
+</body>
+</html>
